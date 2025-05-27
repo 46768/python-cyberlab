4bb229b27d0f344424a5ad449f790e15885be9de
46768 <46768@bodin.ac.th>
2025-05-27 15:33:58 +0700

    feat: add shell properties and run command method

--- Scripts/core/shell/shell.gd
+++ Scripts/core/shell/shell.gd
@@ -2,5 +2,28 @@ class_name Shell
 extends RefCounted
 
 
+# User Info
+var username: String = ""
+var hostname: String = ""
+
+# Environment Info
+var environment_variable: Dictionary[String, String] = {}
+
+# Filesystem Data
+var filesystem: Variant = null
+
+
 func _init() -> void:
+	pass
+
+
+## Run shell commands
+##
+## Program arguments are provided as envvar ("@1", "@2", "@3", ...) for scripts,
+## "@1", "@2", "@3",  ... in SeqASM programs, and args[1:] for gdscript
+## functions
+## Args:
+##	args[0] path to program/script
+##	args[...] program argument
+func run_command(args: PackedStringArray) -> void:
 	pass
