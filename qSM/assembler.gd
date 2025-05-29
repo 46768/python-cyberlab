660f9073d5e2b709d601b3903e5661fd281de295
46768 <46768@bodin.ac.th>
2025-05-29 07:17:11 +0700

    feat: add qsm assembler stub methods

---
+++ qSM/assembler.gd
@@ -0,0 +1,23 @@
+class_name QSMAssembler
+
+
+static func _tokenize(line: String) -> PackedStringArray:
+	pass
+
+
+static func _parse_operand(tokens: PackedStringArray) -> Array:
+	pass
+
+
+static func strip(
+	source: String,
+	rom: Dictionary[String, Variant]
+) -> String:
+	pass
+
+
+static func assemble(
+	source: String,
+	rom: Dictionary[String, Variant]
+) -> Array[Array]:
+	pass
