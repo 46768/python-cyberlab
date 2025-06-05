fee6bb134b08623c44438f8832929491390c8ade
46768 <46768@bodin.ac.th>
2025-06-05 08:48:36 +0700

    feat: add basic shell filesystem command stubs

---
+++ Modules/shell_executable/fs.gd
@@ -0,0 +1,33 @@
+# #####################################################################
+# Navigation
+# #####################################################################
+
+func cd(shell: Shell, args: PackedStringArray) -> Array:
+	pass
+
+
+func ls(shell: Shell, args: PackedStringArray) -> Array:
+	pass
+
+# #####################################################################
+# Block Manipulation
+# #####################################################################
+
+func mkdir(shell: Shell, args: PackedStringArray) -> Array:
+	pass
+
+
+func touch(shell: Shell, args: PackedStringArray) -> Array:
+	pass
+
+
+func rm(shell: Shell, args: PackedStringArray) -> Array:
+	pass
+
+
+func write(shell: Shell, args: PackedStringArray) -> Array:
+	pass
+
+
+func read(shell: Shell, args: PackedStringArray) -> Array:
+	pass
